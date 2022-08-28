e1c8c5c3c4978add6bd6dac8c69efc9735823bc5
n4n5 <its.just.n4n5@gmail.com>
2022-08-28 14:19:04 +0200

    fix: push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const commands = require("./functions");
 const { existsSync: existsSync, mkdirSync: mkdirSync, promises: fs } = require("fs");
 const process = require("process");
-const config = require("./config.json")
+const config = require("./config.json");
 let dev = false;
 
 const run = async () => {
@@ -10,15 +10,15 @@ const run = async () => {
         files.push(...process.argv.slice(3));
         dev = process.argv.indexOf("--dev") === -1 ? false : true;
     }
-    await fs.rmdir(config.buildDir, { recursive: true, force: true });
+    await fs.rm(config.buildDir, { recursive: true, force: true });
     if (!existsSync(config.buildDir)) {
         mkdirSync(config.buildDir);
     }
     const completeMenu = await commands.makeMenu();
-    await fs.writeFile("menu.json", JSON.stringify(completeMenu, null, 4))
+    await fs.writeFile("menu.json", JSON.stringify(completeMenu, null, 4));
     await commands.build(completeMenu, completeMenu, { dev, files });
     await commands.compileCSS();
     await commands.copyDataFolder();
-}
+};
 
 run();
