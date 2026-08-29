b7062b2bcfc1190c3a34a1aaaf5a64c1fd33f121
n4n5 <its.just.n4n5@gmail.com>
2026-08-29 14:41:53 +0200

    add a timout of 2000ms

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,21 @@
 var cacheName = "golb-pwa";
 
 async function networkFirst(request) {
-    try {
-        const networkResponse = await fetch(request);
-        if (networkResponse.ok) {
-            const cache = await caches.open(cacheName);
-            cache.put(request, networkResponse.clone());
-        }
-        return networkResponse;
-    } catch (error) {
-        const cachedResponse = await caches.match(request);
-        return cachedResponse || Response.error();
+  try {
+    const networkResponse = await fetch(request, {
+      signal: AbortSignal.timeout(2000),
+    });
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
     }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
 }
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(networkFirst(event.request));
+  event.respondWith(networkFirst(event.request));
 });
